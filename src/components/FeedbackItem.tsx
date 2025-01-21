c48848af6b0bccdf493892cbd92f5a1bf07d9a36
Ben <ben.kriesel@pm.me>
2025-01-21 16:58:05 +0100

    fix buttons

--- src/components/FeedbackItem.tsx
+++ src/components/FeedbackItem.tsx
@@ -1,15 +1,24 @@
 import { TriangleUpIcon } from "@radix-ui/react-icons";
 import { TFeedbackItem } from "../lib/types";
-
+import { useState } from "react";
 
 type FeedbackItemProps = { feedbackItem: TFeedbackItem };
 
 export default function FeedbackItem({ feedbackItem }: FeedbackItemProps) {
+  const [upvoteCount, setUpvoteCount] = useState(feedbackItem.upvoteCount);
+  const [isDisabled, setIsDisabled] = useState(false);
+
+  const handleUpvote = () => {
+    if (!isDisabled) {
+      setUpvoteCount(upvoteCount + 1);
+      setIsDisabled(true); // Disable button after the upvote
+    }
+  };
   return (
     <li className="feedback">
-      <button>
+      <button onClick={handleUpvote} disabled={isDisabled}>
         <TriangleUpIcon />
-        <span>{feedbackItem.upvoteCount}</span>
+        <span>{upvoteCount}</span>
       </button>
       <div>
         <p>{feedbackItem.badgeLetter}</p>
@@ -19,7 +28,7 @@ export default function FeedbackItem({ feedbackItem }: FeedbackItemProps) {
         <p>{feedbackItem.text}</p>
       </div>
       {/* <p> {feedbackItem.daysAgo}d</p> */}
-      <p> {feedbackItem.daysAgo === 0 ? 'NEW' : `${feedbackItem.daysAgo}d`}</p>
+      <p> {feedbackItem.daysAgo === 0 ? "NEW" : `${feedbackItem.daysAgo}d`}</p>
     </li>
   );
 }
